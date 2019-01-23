167db1e6bacec572a475da8c7b0996c10e4b2e79
StempunkDev <39553418+StempunkDev@users.noreply.github.com>
2019-01-23 14:59:38 +0100

    Import System.IO

--- Midi_Musik Projecct/Main.cs
+++ Midi_Musik Projecct/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace Midi_Musik_Projecct
         public Main()
         {
             InitializeComponent();
+            
         }
     }
 }
