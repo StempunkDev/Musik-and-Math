a18f927c4d1e619573ef8f5b58f17d1dac8a1fd7
StempunkDev <39553418+StempunkDev@users.noreply.github.com>
2019-01-23 14:57:23 +0100

    Rename Form1 File to Main.cs

--- Midi_Musik Projecct/Main.cs
+++ Midi_Musik Projecct/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Midi_Musik_Projecct
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
--- Midi_Musik Projecct/Program.cs
+++ Midi_Musik Projecct/Program.cs
@@ -16,7 +16,7 @@ namespace Midi_Musik_Projecct
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
